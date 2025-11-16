1ff7f20c8475d3ef8bdfdd6d0ac786a168b02d08
ricardo <1968808192@qq.com>
2025-11-16 17:11:59 +0800

    adding system introspection

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,5 +1,5 @@
 #include "mysqrt.h"
-
+#include <cmath>
 #include <iostream>
 
 namespace mathfunctions
@@ -12,7 +12,11 @@ namespace mathfunctions
       {
         return 0;
       }
-
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+      double result = std::exp(std::log(x) * 0.5);
+      std::cout << "Computing sqrt of " << x << " to be " << result
+                << " using log and exp" << std::endl;
+#else
       double result = x;
 
       for (int i = 0; i < 10; ++i)
@@ -25,6 +29,7 @@ namespace mathfunctions
         result = result + 0.5 * delta / result;
         std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
       }
+#endif
       return result;
     }
   }
