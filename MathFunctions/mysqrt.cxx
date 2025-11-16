d7251e39775c48a68fe0dbcaaae38c3c9c27140a
ricardo <1968808192@qq.com>
2025-11-16 17:56:28 +0800

    adding a custom command and generated file

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,5 +1,5 @@
 #include "mysqrt.h"
-#include <cmath>
+#include "Table.h"
 #include <iostream>
 
 namespace mathfunctions
@@ -12,13 +12,12 @@ namespace mathfunctions
       {
         return 0;
       }
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-      double result = std::exp(std::log(x) * 0.5);
-      std::cout << "Computing sqrt of " << x << " to be " << result
-                << " using log and exp" << std::endl;
-#else
       double result = x;
-
+      if (x >= 1 && x < 10)
+      {
+        std::cout << "Use the table to help find an initial value " << std::endl;
+        result = sqrtTable[static_cast<int>(x)];
+      }
       for (int i = 0; i < 10; ++i)
       {
         if (result <= 0)
@@ -29,7 +28,6 @@ namespace mathfunctions
         result = result + 0.5 * delta / result;
         std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
       }
-#endif
       return result;
     }
   }
